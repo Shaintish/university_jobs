cc77f5e9b88eca1bdb12e5889bb690188652ad51
Shaintish <shaintish@mail.ru>
2026-03-03 23:59:58 +0500

    Update services.py

--- app/services.py
+++ app/services.py
@@ -19,3 +19,25 @@ class JobService:
         if search:
             return [v for v in vacancies_db if search.lower() in v.title.lower()]
         return vacancies_db
+    
+     @staticmethod
+    def get_vacancy(vacancy_id: int) -> Vacancy:
+        for v in vacancies_db:
+            if v.id == vacancy_id:
+                return v
+        raise HTTPException(status_code=404, detail="Вакансия не найдена")
+
+    @staticmethod
+    def create_application(data: ApplicationCreate) -> Application:
+        vacancy = next((v for v in vacancies_db if v.id == data.vacancy_id), None)
+        if not vacancy:
+            raise HTTPException(status_code=404, detail="Вакансия не существует")
+
+        new_id = len(applications_db) + 1
+        application = Application(
+            id=new_id,
+            status=ApplicationStatus.PENDING,
+            **data.model_dump()
+        )
+        applications_db.append(application)
+        return application
